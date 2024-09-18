522a44ee64de767f29397a5c036a7b824706430d
Ahumada04 <165733163+ahumada04@users.noreply.github.com>
2024-09-17 18:37:30 -0700

    Add files via upload

---
+++ TestTutor/mainMenuCtrl.java
@@ -0,0 +1,24 @@
+package TestTutor;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+import static TestTutor.SceneSwitch.sceneSwitch;
+
+public class mainMenuCtrl {
+
+    @FXML
+    Button returnBttn;
+
+    public void returnAction(ActionEvent event) throws IOException {
+        sceneSwitch(event, "login.fxml");
+    }
+
+    public void practiceAction(ActionEvent event) throws IOException {
+        sceneSwitch(event, "testSettings.fxml");
+    }
+}
